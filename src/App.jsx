d0f18b5d7541bf16a9f49a3c34e9b08de5ab8a19
Arop Sutra Dhar <142885013+aropdhar@users.noreply.github.com>
2024-03-25 14:22:39 +0600

    result_section_add

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,8 @@ function App() {
 
   });
 
+  let [show , setShow] = useState(true);
+
   let handleinput = (event) => {
  
     let {name , value} = event.target
@@ -74,15 +76,16 @@ function App() {
 
     }
     
-    else if (!text.pass.match(passwordregex)){
+    // else if (!text.pass.match(passwordregex)){
       
-      setError({pass: "Please Enter Your character , Number , symbol submit "});
-    }
+    //   setError({pass: "Please Enter Your character , Number , symbol submit "});
+    // }
     
     else{
 
       setError({pass: " "});
       alert("Submit");
+      setShow(false);
     }
 
   }
@@ -90,17 +93,33 @@ function App() {
   return (
     <>
        <div className="input_main">
-          <input onChange={handleinput} name="firstname" placeholder="Enter Your Name" className="input1"/>
-          <p>{error.firstname}</p>
-          <input onChange={handleinput} name="email" placeholder="Enter Your Email" className="input2"/>
-          <p>{error.email}</p>
-          <input onChange={handleinput} name="address" placeholder="Enter Your Address" className="input3"/>
-          <p>{error.address}</p>
-          <input onChange={handleinput} name="pass" type="password" placeholder="Enter Your password" className="input4"/>
-          <p>{error.pass}</p>
-          <button onClick={handlebtn} className="btn">Submit</button>
+        {show ?
+            <div className="input_card">
+                <input onChange={handleinput} name="firstname" placeholder="Enter Your Name" className="input1"/>
+                <p>{error.firstname}</p>
+                <input onChange={handleinput} name="email" placeholder="Enter Your Email" className="input2"/>
+                <p>{error.email}</p>
+                <input onChange={handleinput} name="address" placeholder="Enter Your Address" className="input3"/>
+                <p>{error.address}</p>
+                <input onChange={handleinput} name="pass" type="password" placeholder="Enter Your password" className="input4"/>
+                <p>{error.pass}</p>
+                <button onClick={handlebtn} className="btn">Submit</button>
+            </div>
+         
+         :
+
+              
+              <div className="input_result">
+                  <h2>Your Name: <p>{text.firstname}</p></h2>
+                  <h2>Your Email: <p>{text.email}</p></h2>
+                  <h2>Your Address: <p>{text.address}</p></h2>
+                  <h2>Your Password: <p>{text.pass}</p></h2>
+              </div>
+
+        }
+
        </div>
-      
+
     </>
   )
 }
