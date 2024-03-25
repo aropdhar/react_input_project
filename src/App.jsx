cf80e473f00a97d09330e00af2a60a95abb73145
Arop Sutra Dhar <142885013+aropdhar@users.noreply.github.com>
2024-03-25 13:22:59 +0600

    react_project

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react"
+
+
+
+function App() {
+  
+  let nameregex = /^[a-zA-Z ]{2,40}$/ ;
+  let emailregex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let passwordregex =  /^[@#][A-Za-z0-9]{7,13}$/ ;
+
+  let [text , setText] = useState({
+
+    firstname: "",
+    email: "",
+    address: "",
+    pass: ""
+
+  });
+  
+  let [error , setError] = useState({
+
+    firstname: "",
+    email: "",
+    address: "",
+    pass: "",
+
+  });
+
+  let handleinput = (event) => {
+ 
+    let {name , value} = event.target
+    setText({...text , [name]:value});
+    
+  }
+
+  let handlebtn = () => {
+
+    if (text.firstname == ""){
+ 
+      setError({firstname: "Please Enter Your Name"});
+      
+    }
+
+    else if(!text.firstname.match(nameregex)){
+      
+      setError({firstname: "Please enter your valid name"});
+      
+    }
+    
+    else if(text.email == ""){
+      
+      setError({firstname: ""});
+      setError({email: "Please Enter Your Email"});
+
+    }
+
+    else if(!text.email.match(emailregex)){
+     
+      setError({email: "Please Enter Your Valid Email"});
+
+    }
+    
+    else if(text.address == ""){
+
+      setError({email: ""});
+      setError({address: "Please Enter Your Address"});
+
+    }
+    
+    else if(text.pass == ""){
+   
+      setError({address: ""});
+      setError({pass: "Please Enter Your password"});
+
+    }
+    
+    else if (!text.pass.match(passwordregex)){
+      
+      setError({pass: "Please Enter Your character , Number , symbol submit "});
+    }
+    
+    else{
+
+      setError({pass: " "});
+      alert("Submit");
+    }
+
+  }
+
+  return (
+    <>
+       <div className="input_main">
+          <input onChange={handleinput} name="firstname" placeholder="Enter Your Name" className="input1"/>
+          <p>{error.firstname}</p>
+          <input onChange={handleinput} name="email" placeholder="Enter Your Email" className="input2"/>
+          <p>{error.email}</p>
+          <input onChange={handleinput} name="address" placeholder="Enter Your Address" className="input3"/>
+          <p>{error.address}</p>
+          <input onChange={handleinput} name="pass" type="password" placeholder="Enter Your password" className="input4"/>
+          <p>{error.pass}</p>
+          <button onClick={handlebtn} className="btn">Submit</button>
+       </div>
+      
+    </>
+  )
+}
+
+export default App
